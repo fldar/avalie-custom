770731105a61a5f2b432d5060efca912127c6fdd
fldar <lucasalmeidacco@gmail.com>
2017-11-02 20:21:40 -0300

    .

--- resources/views/agradecimento.blade.php
+++ resources/views/agradecimento.blade.php
@@ -1,7 +1,1 @@
-<h2>Email de Elogio</h2>
-	<br>
-	<p>Técnico: {{$dados->tecnico}}</p>
-	<p>Empresa: {{$dados->registro}}</p>
-	<p>Elogio: {{$dados->elogio}}</p>
-	<br>
-	<p>Feita por: {{$dados->email}}</p>
+<h2>Obrigador por avaliar!!!!</h2>
