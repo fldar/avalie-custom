3974b89058e0c569749fd6fbc4a865f7f7547a8d
fldar <lucasalmeidacco@gmail.com>
2017-11-02 15:09:58 -0300

    Fresh Elogio

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail; 
 use App\Mail\ContatoEmail;
+use App\Mail\ContatoElogio;
 
 
 class ContatoController extends Controller
