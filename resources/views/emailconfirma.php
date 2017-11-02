814d6a7ffd7b18a3fc34188a33cc25c52053f4f9
fldar <lucasalmeidacco@gmail.com>
2017-11-02 19:54:28 -0300

    .

--- resources/views/emailconfirma.php
+++ resources/views/emailconfirma.php
@@ -1,13 +1,8 @@
-<h2>Você avaliou o técnico {{$dados->tecnico}}<h2>
+<h2>Você avaliou o técnico {{$dados->tecnico}}</h2>
 	<br>
 	<p>Cortesia: {{$dados->cortesia}}</p>
 	<p>Eficacia: {{$dados->eficacia}}</p>
 	<p>Suporte: {{$dados->suporte}}</p>
-<<<<<<< HEAD
-	<p>Indicaria a Softcom: {{$dados->indicaria}}</p>
-=======
-	<br>
 	<p>Indicaria a Softcom: {{$dados->indicaria}}</p>
 	<br>
->>>>>>> 1dd4bf0693d58ead115a1661cb4aa2fef6244f0a
 	<p>Caso não tenha feito essa avaliação entrar em contato.</p>
