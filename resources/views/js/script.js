85c04d25399b46ee24abf812377de086bd9e6b25
fldar <lucasalmeidacco@gmail.com>
2017-11-02 22:27:45 -0300

    .

--- resources/views/js/script.js
+++
@@ -1,8 +0,0 @@
-if ($nome && validaEmail($de) && $mensagem) {
-    enviaEmail($de, $assunto, $mensagem, $para, $email_servidor);
-    $pagina = "mail_ok.php";
-} else {
-    $pagina = "mail_error.php";
-}
-
-header("location:$pagina");
