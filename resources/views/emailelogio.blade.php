99631487f913f7edad7715c1663a0733dd1cb20e
fldar <lucasalmeidacco@gmail.com>
2017-11-03 09:20:36 -0300

    .

--- resources/views/emailelogio.blade.php
+++ resources/views/emailelogio.blade.php
@@ -1,8 +1,9 @@
 <h2>Elogio enviado por email</h2>
+<h3>
 Empresa: <span style="font-style: italic">{{$dados->registro}} </span>
 | 
 Técnico: <span style="font-style: italic">{{$dados->tecnico}} </span>
-</h3	>
+</h3>
 <br>
 <p>Elogio:</p>
 <p style="font-style: italic">&ldquo;{{$dados->elogio}}&rdquo;</p>
